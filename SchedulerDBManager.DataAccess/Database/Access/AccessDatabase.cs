dccdfabea4946850ac08f42c9c25211cc3bc0b12
Timofei Irishiin <eblan2204.1@gmail.com>
2026-05-28 08:54:17 +0300

    Рефактор класса AccessDatabase - Декомпозиция методов для работы с бд: добавлен метод GetConnection() для установления подключения, добавлен метод GetCommand() для получения оператора для работы с БД - В конструктор класса внедрена возможность устанавливать путь к файлу

--- SchedulerDBManager.DataAccess/Database/Access/AccessDatabase.cs
+++ SchedulerDBManager.DataAccess/Database/Access/AccessDatabase.cs
@@ -8,12 +8,23 @@ namespace SchedulerDBManager.DataAccess.Database.Access
 {
     public class AccessDatabase : IDatabase
     {
-        private readonly string connectionString = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=ShiftSchedule.accdb;";
+
+        private readonly string connectionString;
+
+        public AccessDatabase(string databasePath = "ShiftSchedule.accdb")
+        {
+            if (string.IsNullOrWhiteSpace(databasePath))
+                throw new ArgumentException("Путь к файлу базы данных не может быть пустым", nameof(databasePath));
+
+            connectionString = $"Provider=Microsoft.ACE.OLEDB.12.0;Data Source={databasePath};";
+        }
 
         public DataTable ExecuteSelect(string query, params OleDbParameter[] parameters)
         {
-            using (var conn = new OleDbConnection(connectionString))
-            using (var cmd = new OleDbCommand(query, conn))
+            ValidateQuery(query);
+
+            using (var conn = GetConnection())
+            using (var cmd = GetCommand(query, conn, parameters))
             {
                 if (parameters != null) cmd.Parameters.AddRange(parameters);
                 var dt = new DataTable();
@@ -24,13 +35,33 @@ namespace SchedulerDBManager.DataAccess.Database.Access
 
         public void ExecuteNonQuery(string query, params OleDbParameter[] parameters)
         {
-            using (var conn = new OleDbConnection(connectionString))
-            using (var cmd = new OleDbCommand(query, conn))
+            ValidateQuery(query);
+
+            using (var conn = GetConnection())
+            using (var cmd = GetCommand(query, conn, parameters))
             {
                 if (parameters != null) cmd.Parameters.AddRange(parameters);
                 conn.Open();
                 cmd.ExecuteNonQuery();
             }
+        }
+
+        private OleDbConnection GetConnection()
+        {
+            return new OleDbConnection(connectionString);
+        }
+
+        private OleDbCommand GetCommand(string query, OleDbConnection connection, params OleDbParameter[] parameters)
+        {
+            var cmd = new OleDbCommand(query, connection);
+            if (parameters != null) cmd.Parameters.AddRange(parameters);
+            return cmd;
+        }
+
+        private void ValidateQuery(string query)
+        {
+            if (string.IsNullOrWhiteSpace(query))
+                throw new ArgumentException("Запрос не может быть пустым", nameof(query));
         }
     }
 }
