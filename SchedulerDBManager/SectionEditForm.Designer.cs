557090f9eac995322398a1849e2dc1e222755d47
Timofei Irishiin <eblan2204.1@gmail.com>
2026-06-03 22:47:51 +0300

    Реализовано UI для редактирования участков

---
+++ SchedulerDBManager/SectionEditForm.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace SchedulerDBManager.Presentation
+{
+    partial class SectionEditForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            tableLayout = new TableLayoutPanel();
+            addressLbl = new Label();
+            phoneLbl = new Label();
+            sectionLbl = new Label();
+            sectionAddress = new ComboBox();
+            addressField = new TextBox();
+            phoneField = new TextBox();
+            btnSave = new Button();
+            btnCancel = new Button();
+            tableLayout.SuspendLayout();
+            SuspendLayout();
+            // 
+            // tableLayout
+            // 
+            tableLayout.ColumnCount = 2;
+            tableLayout.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tableLayout.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tableLayout.Controls.Add(phoneField, 1, 1);
+            tableLayout.Controls.Add(addressLbl, 0, 0);
+            tableLayout.Controls.Add(phoneLbl, 0, 1);
+            tableLayout.Controls.Add(sectionLbl, 0, 2);
+            tableLayout.Controls.Add(btnCancel, 0, 3);
+            tableLayout.Controls.Add(btnSave, 1, 3);
+            tableLayout.Controls.Add(sectionAddress, 1, 2);
+            tableLayout.Controls.Add(addressField, 1, 0);
+            tableLayout.Dock = DockStyle.Fill;
+            tableLayout.Location = new Point(0, 0);
+            tableLayout.Name = "tableLayout";
+            tableLayout.RowCount = 4;
+            tableLayout.RowStyles.Add(new RowStyle(SizeType.Percent, 25F));
+            tableLayout.RowStyles.Add(new RowStyle(SizeType.Percent, 25.0000038F));
+            tableLayout.RowStyles.Add(new RowStyle(SizeType.Percent, 25F));
+            tableLayout.RowStyles.Add(new RowStyle(SizeType.Percent, 25.0000038F));
+            tableLayout.RowStyles.Add(new RowStyle(SizeType.Absolute, 20F));
+            tableLayout.Size = new Size(800, 450);
+            tableLayout.TabIndex = 0;
+            // 
+            // addressLbl
+            // 
+            addressLbl.AutoSize = true;
+            addressLbl.Dock = DockStyle.Bottom;
+            addressLbl.Location = new Point(3, 92);
+            addressLbl.Name = "addressLbl";
+            addressLbl.Size = new Size(394, 20);
+            addressLbl.TabIndex = 0;
+            addressLbl.Text = "Адрес:";
+            // 
+            // phoneLbl
+            // 
+            phoneLbl.AutoSize = true;
+            phoneLbl.Dock = DockStyle.Bottom;
+            phoneLbl.Location = new Point(3, 204);
+            phoneLbl.Name = "phoneLbl";
+            phoneLbl.Size = new Size(394, 20);
+            phoneLbl.TabIndex = 1;
+            phoneLbl.Text = "Номер телефона:";
+            // 
+            // sectionLbl
+            // 
+            sectionLbl.AutoSize = true;
+            sectionLbl.Dock = DockStyle.Bottom;
+            sectionLbl.Location = new Point(3, 316);
+            sectionLbl.Name = "sectionLbl";
+            sectionLbl.Size = new Size(394, 20);
+            sectionLbl.TabIndex = 4;
+            sectionLbl.Text = "Подразделение:";
+            // 
+            // sectionAddress
+            // 
+            sectionAddress.Dock = DockStyle.Bottom;
+            sectionAddress.FormattingEnabled = true;
+            sectionAddress.Location = new Point(403, 305);
+            sectionAddress.Name = "sectionAddress";
+            sectionAddress.Size = new Size(394, 28);
+            sectionAddress.TabIndex = 11;
+            // 
+            // addressField
+            // 
+            addressField.Dock = DockStyle.Bottom;
+            addressField.Location = new Point(403, 82);
+            addressField.Name = "addressField";
+            addressField.Size = new Size(394, 27);
+            addressField.TabIndex = 12;
+            // 
+            // phoneField
+            // 
+            phoneField.Dock = DockStyle.Bottom;
+            phoneField.Location = new Point(403, 194);
+            phoneField.Name = "phoneField";
+            phoneField.Size = new Size(394, 27);
+            phoneField.TabIndex = 13;
+            // 
+            // btnSave
+            // 
+            btnSave.Dock = DockStyle.Bottom;
+            btnSave.Location = new Point(440, 387);
+            btnSave.Margin = new Padding(40, 10, 40, 10);
+            btnSave.Name = "btnSave";
+            btnSave.Size = new Size(320, 53);
+            btnSave.TabIndex = 6;
+            btnSave.Text = "Сохранить";
+            btnSave.UseVisualStyleBackColor = true;
+            // 
+            // btnCancel
+            // 
+            btnCancel.Dock = DockStyle.Bottom;
+            btnCancel.Location = new Point(40, 387);
+            btnCancel.Margin = new Padding(40, 10, 40, 10);
+            btnCancel.Name = "btnCancel";
+            btnCancel.Size = new Size(320, 53);
+            btnCancel.TabIndex = 5;
+            btnCancel.Text = "Отмена";
+            btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // SectionEditForm
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(tableLayout);
+            Name = "SectionEditForm";
+            Text = "ScheduleEditForm";
+            tableLayout.ResumeLayout(false);
+            tableLayout.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private TableLayoutPanel tableLayout;
+        private Label addressLbl;
+        private Label phoneLbl;
+        private Label sectionLbl;
+        private ComboBox sectionAddress;
+        private TextBox addressField;
+        private TextBox phoneField;
+        private Button btnCancel;
+        private Button btnSave;
+    }
+}
---
+++ SchedulerDBManager/SectionsForm.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace SchedulerDBManager.Presentation
+{
+    partial class SectionsForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            dvgSchedules = new DataGridView();
+            splitContainer = new SplitContainer();
+            tableLayoutPanel = new TableLayoutPanel();
+            btnHelp = new Button();
+            btnDelete = new Button();
+            btnEdit = new Button();
+            btnAdd = new Button();
+            contextMenuStrip1 = new ContextMenuStrip(components);
+            ((System.ComponentModel.ISupportInitialize)dvgSchedules).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)splitContainer).BeginInit();
+            splitContainer.Panel1.SuspendLayout();
+            splitContainer.Panel2.SuspendLayout();
+            splitContainer.SuspendLayout();
+            tableLayoutPanel.SuspendLayout();
+            SuspendLayout();
+            // 
+            // dvgSchedules
+            // 
+            dvgSchedules.BackgroundColor = SystemColors.Control;
+            dvgSchedules.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dvgSchedules.Dock = DockStyle.Fill;
+            dvgSchedules.Location = new Point(0, 0);
+            dvgSchedules.Name = "dvgSchedules";
+            dvgSchedules.RowHeadersWidth = 51;
+            dvgSchedules.Size = new Size(546, 450);
+            dvgSchedules.TabIndex = 0;
+            // 
+            // splitContainer
+            // 
+            splitContainer.Dock = DockStyle.Fill;
+            splitContainer.Location = new Point(0, 0);
+            splitContainer.Name = "splitContainer";
+            // 
+            // splitContainer.Panel1
+            // 
+            splitContainer.Panel1.Controls.Add(tableLayoutPanel);
+            // 
+            // splitContainer.Panel2
+            // 
+            splitContainer.Panel2.Controls.Add(dvgSchedules);
+            splitContainer.Size = new Size(800, 450);
+            splitContainer.SplitterDistance = 250;
+            splitContainer.TabIndex = 1;
+            // 
+            // tableLayoutPanel
+            // 
+            tableLayoutPanel.ColumnCount = 1;
+            tableLayoutPanel.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100F));
+            tableLayoutPanel.Controls.Add(btnHelp, 0, 3);
+            tableLayoutPanel.Controls.Add(btnDelete, 0, 2);
+            tableLayoutPanel.Controls.Add(btnEdit, 0, 1);
+            tableLayoutPanel.Controls.Add(btnAdd, 0, 0);
+            tableLayoutPanel.Dock = DockStyle.Fill;
+            tableLayoutPanel.Location = new Point(0, 0);
+            tableLayoutPanel.Name = "tableLayoutPanel";
+            tableLayoutPanel.RowCount = 4;
+            tableLayoutPanel.RowStyles.Add(new RowStyle(SizeType.Percent, 25F));
+            tableLayoutPanel.RowStyles.Add(new RowStyle(SizeType.Percent, 25F));
+            tableLayoutPanel.RowStyles.Add(new RowStyle(SizeType.Percent, 25F));
+            tableLayoutPanel.RowStyles.Add(new RowStyle(SizeType.Percent, 25F));
+            tableLayoutPanel.RowStyles.Add(new RowStyle(SizeType.Absolute, 20F));
+            tableLayoutPanel.RowStyles.Add(new RowStyle(SizeType.Absolute, 20F));
+            tableLayoutPanel.RowStyles.Add(new RowStyle(SizeType.Absolute, 20F));
+            tableLayoutPanel.RowStyles.Add(new RowStyle(SizeType.Absolute, 20F));
+            tableLayoutPanel.RowStyles.Add(new RowStyle(SizeType.Absolute, 20F));
+            tableLayoutPanel.Size = new Size(250, 450);
+            tableLayoutPanel.TabIndex = 0;
+            // 
+            // btnHelp
+            // 
+            btnHelp.Dock = DockStyle.Bottom;
+            btnHelp.Location = new Point(30, 391);
+            btnHelp.Margin = new Padding(30);
+            btnHelp.Name = "btnHelp";
+            btnHelp.Size = new Size(190, 29);
+            btnHelp.TabIndex = 3;
+            btnHelp.Text = "Справка";
+            btnHelp.UseVisualStyleBackColor = true;
+            // 
+            // btnDelete
+            // 
+            btnDelete.Dock = DockStyle.Bottom;
+            btnDelete.Location = new Point(30, 277);
+            btnDelete.Margin = new Padding(30);
+            btnDelete.Name = "btnDelete";
+            btnDelete.Size = new Size(190, 29);
+            btnDelete.TabIndex = 2;
+            btnDelete.Text = "Удалить участок";
+            btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // btnEdit
+            // 
+            btnEdit.Dock = DockStyle.Bottom;
+            btnEdit.Location = new Point(30, 165);
+            btnEdit.Margin = new Padding(30);
+            btnEdit.Name = "btnEdit";
+            btnEdit.Size = new Size(190, 29);
+            btnEdit.TabIndex = 1;
+            btnEdit.Text = "Редактировать участок";
+            btnEdit.UseVisualStyleBackColor = true;
+            // 
+            // btnAdd
+            // 
+            btnAdd.Dock = DockStyle.Bottom;
+            btnAdd.Location = new Point(30, 53);
+            btnAdd.Margin = new Padding(30);
+            btnAdd.Name = "btnAdd";
+            btnAdd.Size = new Size(190, 29);
+            btnAdd.TabIndex = 0;
+            btnAdd.Text = "Создать участок";
+            btnAdd.UseVisualStyleBackColor = true;
+            // 
+            // contextMenuStrip1
+            // 
+            contextMenuStrip1.ImageScalingSize = new Size(20, 20);
+            contextMenuStrip1.Name = "contextMenuStrip1";
+            contextMenuStrip1.Size = new Size(61, 4);
+            // 
+            // SectionsForm
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(splitContainer);
+            Name = "SectionsForm";
+            Text = "Schedule";
+            ((System.ComponentModel.ISupportInitialize)dvgSchedules).EndInit();
+            splitContainer.Panel1.ResumeLayout(false);
+            splitContainer.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)splitContainer).EndInit();
+            splitContainer.ResumeLayout(false);
+            tableLayoutPanel.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private DataGridView dvgSchedules;
+        private SplitContainer splitContainer;
+        private TableLayoutPanel tableLayoutPanel;
+        private ContextMenuStrip contextMenuStrip1;
+        private Button btnDelete;
+        private Button btnEdit;
+        private Button btnAdd;
+        private Button btnHelp;
+    }
+}
